0df39c106f922d419f0af3dd4ff873677f7c2420
barisdinc <barisdinc@gmail.com>
2017-01-21 12:00:58 +0300

    VNA power presets 2

--- ASELSAN_4822.ino
+++ ASELSAN_4822.ino
@@ -564,7 +564,7 @@ void readRfPower()
 {
    int refPower = 0;
    int fwdPower = 0;
-  //TODO: Set RF power to LOW for this operation, and restore user setting on exit..   
+   
    fwdPower = analogRead(FWD_POWER_PIN);
    refPower = analogRead(REF_POWER_PIN);
    refPower = refPower * 2;
@@ -587,9 +587,7 @@ void readRfPower()
    {
       highestFRQ=calc_frequency;
    }
-     
-     
-  
+
 }
 
 
@@ -958,7 +956,7 @@ void loop() {
             for (long vna_freq=14000; vna_freq < 15000; vna_freq += 10)
               {
                 TRX_MODE = TX;
-                Serial.print(vna_freq); 
+                //Serial.print(vna_freq); 
                 numberToFrequency(vna_freq*10,FRQ);
                 validFRQ = Calculate_Frequency(FRQ);
                 write_FRQ(calc_frequency);
